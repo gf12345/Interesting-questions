c819c67cc1f1a3e186f99ff811e5006d40e4d3e7
gf123456 <1305088645@qq.com>
2021-02-22 09:31:16 +0800

    红包问题的实现(C++)

--- test.cpp
+++ test.cpp
@@ -1,11 +1,11 @@
-#include<map>
-#include<vector>
 #include<iostream>
+#include<vector>
+#include<map>
 using namespace std;
-//�����ڼ�С��ʹ��΢���յ��ܶ��������ǳ����ġ��ڲ鿴��ȡ�����¼ʱ���֣�
-//ĳ����������ֵĴ��������˺��������һ�롣
-//���С���ҵ��ú����д�������㷨˼·�ʹ���ʵ�֣�Ҫ���㷨�����ܸ�Ч��
-//����һ������Ľ������gifts�����Ĵ�Сn���뷵���������Ľ�
+//春节期间小明使用微信收到很多个红包，非常开心。在查看领取红包记录时发现，
+//某个红包金额出现的次数超过了红包总数的一半。
+//请帮小明找到该红包金额。写出具体算法思路和代码实现，要求算法尽可能高效。
+//给定一个红包的金额数组gifts及它的大小n，请返回所求红包的金额。
 int getValue(vector<int> gifts, int n){
 	if (gifts.empty())
 		return 0;
